cceb794f111a4294b0ec7e6bb43db381f1b2e038
rajeshbhakarr <bhakarjaat17@gmail.com>
2026-09-01 15:43:13 +0530

    fix 1 min wingo mode

--- src/pages/Aviator.jsx
+++ src/pages/Aviator.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useWallet } from "../context/WalletContext";
 import Swal from "sweetalert2";
-import "./aviator.css";
+import "../styles/aviator.css";
 
 const API = "https://indr-backend-77tp.onrender.com/api";
 
