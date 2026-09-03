cfb5a72e048feeb2bb0e37c6003f042e71c511f7
rajeshbhakarr <bhakarjaat17@gmail.com>
2026-09-03 15:03:58 +0530

    Restore Aviator CSS

--- src/pages/FortuneGems.jsx
+++ src/pages/FortuneGems.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useWallet } from "../context/WalletContext";
 import Swal from "sweetalert2";
-import "..styles/fortuneGems.css";
+import "../styles/fortuneGems.css";
 
 // 🔥 ADD THIS IMPORT ONLY
 import * as fortuneGemsApi from "../api/fortuneGemsApi";
