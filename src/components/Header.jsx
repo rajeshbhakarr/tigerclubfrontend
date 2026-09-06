67500e7212a6fd1d716aa623d319f6438b815277
rajeshbhakarr <bhakarjaat17@gmail.com>
2026-09-06 09:00:56 +0530

    Restore Aviator CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../styles/header.css";
 function Header() {
   return (
     <div>
-      <div className="in">Tigerclub</div>
+      <div className="in">RAJA WIN</div>
 
       <div className="img">
         <img src="/bannerr.png" alt="not found" width={800} height={300} />
